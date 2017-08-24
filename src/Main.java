119637b0ca52ef619f4e9d34634c8c63d4a3a84d
George Thayamkery <george.thayamkery@gmail.com>
2017-08-24 16:26:47 -0500

    init

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,17 @@
  * Created by 164543 on 8/24/2017.
  */
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Main extends StateBasedGame {
     public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void initStatesList(GameContainer gameContainer) throws SlickException {
 
     }
 }
